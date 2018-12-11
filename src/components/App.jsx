49f43e77978740c9a192994a165b4ce4ac0dc7e6
Donte' Nall <code4zelda@gmail.com>
2018-12-10 19:44:07 -0800

    setup webpack and react

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: ""
+        }
+    }
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+
+export default App;
